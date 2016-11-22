4565d208edb1334b5c2a6798bce978032de7d620
Imsaho <jo.bednarczyk@gmail.com>
2016-11-22 20:15:21 +0100

    implemented FOSUserBundle

--- src/BubbleDiagramBundle/Entity/User.php
+++ src/BubbleDiagramBundle/Entity/User.php
@@ -5,6 +5,7 @@ namespace BubbleDiagramBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use BubbleDiagramBundle\Entity\Team;
+use FOS\UserBundle\Model\User as BaseUser;
 
 /**
  * User
@@ -12,8 +13,8 @@ use BubbleDiagramBundle\Entity\Team;
  * @ORM\Table(name="user")
  * @ORM\Entity(repositoryClass="BubbleDiagramBundle\Repository\UserRepository")
  */
-class User
-{
+class User extends BaseUser {
+
     /**
      * @var int
      *
@@ -21,87 +22,26 @@ class User
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
+    protected $id;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="email", type="string", length=64, unique=true)
-     */
-    private $email;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="password", type="string", length=255)
-     */
-    private $password;
-    
     /**
      * @ORM\ManyToMany(targetEntity="Team", inversedBy="users")
      * @ORM\JoinTable(name="users_teams")
      */
     private $teams;
-    
+
     public function __construct() {
+        parent:: __construct();
         $this->teams = new ArrayCollection();
     }
-
 
     /**
      * Get id
      *
      * @return integer 
      */
-    public function getId()
-    {
+    public function getId() {
         return $this->id;
-    }
-
-    /**
-     * Set email
-     *
-     * @param string $email
-     * @return User
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    /**
-     * Get email
-     *
-     * @return string 
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    /**
-     * Set password
-     *
-     * @param string $password
-     * @return User
-     */
-    public function setPassword($password)
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    /**
-     * Get password
-     *
-     * @return string 
-     */
-    public function getPassword()
-    {
-        return $this->password;
     }
 
     /**
@@ -110,8 +50,7 @@ class User
      * @param \BubbleDiagramBundle\Entity\Team $teams
      * @return User
      */
-    public function addTeam(\BubbleDiagramBundle\Entity\Team $teams)
-    {
+    public function addTeam(\BubbleDiagramBundle\Entity\Team $teams) {
         $this->teams[] = $teams;
 
         return $this;
@@ -122,8 +61,7 @@ class User
      *
      * @param \BubbleDiagramBundle\Entity\Team $teams
      */
-    public function removeTeam(\BubbleDiagramBundle\Entity\Team $teams)
-    {
+    public function removeTeam(\BubbleDiagramBundle\Entity\Team $teams) {
         $this->teams->removeElement($teams);
     }
 
@@ -132,12 +70,12 @@ class User
      *
      * @return \Doctrine\Common\Collections\Collection 
      */
-    public function getTeams()
-    {
+    public function getTeams() {
         return $this->teams;
     }
-    
+
     public function __toString() {
         return $this->email;
     }
+
 }

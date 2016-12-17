46ce0d3f9506b00524d010d411850a4218e110c5
Imsaho <jo.bednarczyk@gmail.com>
2016-12-17 22:27:15 +0100

    changed: master

--- src/BubbleDiagramBundle/Controller/BuildingController.php
+++ src/BubbleDiagramBundle/Controller/BuildingController.php
@@ -9,9 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Response;
-//use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\File\Exception\AccessDeniedException;
-use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
 
 /**
  * Building controller.

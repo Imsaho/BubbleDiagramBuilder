848ca28c1551d93e4f70e713a5af6ab7d6fcf925
Imsaho <jo.bednarczyk@gmail.com>
2017-01-11 15:38:36 +0100

    created zone bubbleChart using raphael drawing library

--- web/js/bubbleChart.js
+++ web/js/bubbleChart.js
@@ -2,54 +2,50 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     var canvas = document.getElementById("chart");
+    var paper = new Raphael(canvas, 1500, 600);
     var chartItems = document.querySelectorAll('tr[data-chart="true"]');
 
     function drawBubbleChart(numberOfSides, size, Xcenter, Ycenter, allItemsData) {
-        cxt.moveTo(Xcenter + size * Math.cos(0),
-                Ycenter + size * Math.sin(0));
 
         for (var i = 1; i <= numberOfSides; i++) {
 
-            cxt.beginPath();
-            cxt.fillStyle = allItemsData[i - 1]["color"];
-            var radius = Math.sqrt(allItemsData[i-1]["chartArea"] / Math.PI )*35;
+            var radius = Math.sqrt(allItemsData[i - 1]["chartArea"] / Math.PI) * 35;
+            var info = allItemsData[i - 1]["name"];
 
-            cxt.moveTo(Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides),
-                    Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides));
+            paper.circle(Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides),
+                    Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides), radius)
+                    .attr({fill: allItemsData[i - 1]["color"], stroke: "none", opacity: 0.85})
+                    .data("name", allItemsData[i-1]["name"])
+                    .data("area", allItemsData[i-1]["area"])
+                    .click(function () {
 
-            cxt.arc(Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides),
-                    Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides), radius, 0, 2 * Math.PI);
-            cxt.fill();
+                        console.log(this.data("name") + ", "
+                + this.data("area") + (' m\xB2'));
+                    });
         }
     }
 
-    if (canvas.getContext("2d")) {
-        var cxt = canvas.getContext("2d");
-        cxt.globalAlpha = 0.8;
-
-        var allItemsData = [];
-        for (var i = 0; i < chartItems.length; i++) {
-            allItemsData.push(chartItems[i].dataset);
-        }
-//        console.log(allItemsData);
-        
-        var totalArea = 0;
-        for (var i=0; i<allItemsData.length; i++) {
-            totalArea += Number(allItemsData[i]['area']);
-        }
-//        console.log(totalArea);
-        
-        for (var i=0; i<allItemsData.length; i++) {
-            allItemsData[i]['chartArea'] = allItemsData[i]['area'] / totalArea * 100;
-        }
-        
-//        console.log(allItemsData);
-
-        var numberOfSides = chartItems.length,
-                size = 170,
-                Xcenter = 300,
-                Ycenter = 300;
-
-        drawBubbleChart(numberOfSides, size, Xcenter, Ycenter, allItemsData);
+    var allItemsData = [];
+    for (var i = 0; i < chartItems.length; i++) {
+        allItemsData.push(chartItems[i].dataset);
     }
+
+    var totalArea = 0;
+    for (var i = 0; i < allItemsData.length; i++) {
+        totalArea += Number(allItemsData[i]['area']);
+    }
+
+    for (var i = 0; i < allItemsData.length; i++) {
+        allItemsData[i]['chartArea'] = allItemsData[i]['area'] / totalArea * 100;
+    }
+
+    console.log(allItemsData);
+
+    var numberOfSides = chartItems.length,
+            size = 170,
+            Xcenter = 300,
+            Ycenter = 300;
+
+    drawBubbleChart(numberOfSides, size, Xcenter, Ycenter, allItemsData);
+
 });
